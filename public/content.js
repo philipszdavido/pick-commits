abf4b114699bac1feaab489e8afe5abf44f5a01a
philipszdavido <kurtwanger40@gmail.com>
2022-11-13 22:06:53 +0100

    added files

---
+++ public/content.js
@@ -0,0 +1,70 @@
+const messagesFromReactAppListener = (message, sender, response) => {
+  const searchBody = (body, searchToken = "") => {
+    // break the token
+    const searchTokens = searchToken
+      .split(",")
+      .map((token) => token.trim().toLowerCase());
+    try {
+      const results = [];
+      const table = body.getElementsByClassName("commits-table")?.[0];
+      const tableBody = table.getElementsByTagName("tbody")?.[0];
+      const tableRows = tableBody.getElementsByTagName("tr");
+
+      // iterate through table rows.
+      for (const row of tableRows) {
+        // Discard row that has class 'merge'
+        if (
+          row.getAttribute("class") !== "merge" &&
+          row.getAttribute("class").split(" ").includes("merge") === false
+        ) {
+          const tdAuthor = row.getElementsByClassName("author")[0];
+
+          // const userName = tdAuthor.getElementsByClassName("user-name")[0].textContent
+          const userName = tdAuthor.innerText;
+
+          // check if the author is the commit author
+          const match = patternMatch(
+            searchTokens,
+            userName.trim().toLowerCase()
+          );
+          if (match) {
+            const tdCommit = row.getElementsByClassName("commit")[0];
+            const href = tdCommit
+              .getElementsByTagName("a")[0]
+              .getAttribute("href");
+            const hrefParts = href.split("/");
+            const commit = hrefParts[hrefParts.length - 1];
+            results.push({
+              userName,
+              commit,
+            });
+          }
+        }
+      }
+      return results;
+    } catch (error) {
+      return [];
+    }
+  };
+
+  const patternMatch = (tokens, username) => {
+    for (let index = 0; index < tokens.length; index++) {
+      const token = tokens[index];
+      const regex = new RegExp(token, "g");
+      const match = regex.exec(username.toLowerCase());
+      if (match?.length > 0) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  if (sender.id === chrome.runtime.id && message.from === "PickCommits") {
+    response(searchBody(document.body, message.search));
+  }
+};
+
+/**
+ * Fired when a message is sent from either an extension process or a content script.
+ */
+chrome.runtime.onMessage.addListener(messagesFromReactAppListener);
--- src/App.js
+++ src/App.js
@@ -1,23 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ShowCommits } from "./ShowCommits";
+import icon from "./icon.png";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [commitResults, setCommitsResult] = useState(null);
+  const [author, setAuthor] = useState();
+  const [alert, setAlert] = useState({
+    message: "",
+    type: "",
+    show: false,
+  });
+
+  const interval = useRef();
+
+  const handleSearchFn = () => {
+    setLoading(true);
+
+    const message = {
+      from: "PickCommits",
+      search: author,
+    };
+
+    const queryInfo = {
+      active: true,
+      currentWindow: true,
+    };
+
+    chrome.tabs &&
+      chrome.tabs.query(queryInfo, (tabs) => {
+        const currentTabId = tabs[0].id;
+        chrome.tabs.sendMessage(currentTabId, message, (response) => {
+          getResponse(response);
+        });
+      });
+  };
+
+  const getResponse = (results) => {
+    setLoading(false);
+    setCommitsResult(results);
+  };
+
+  const handleOnChangeFn = (evt) => {
+    setAuthor(evt.target.value);
+  };
+
+  const handleKeyUpFn = (evt) => {
+    const { key, keyCode } = evt;
+    if (key === "Enter" && keyCode === 13) {
+      handleSearchFn();
+    }
+  };
+
+  const goBack = () => {
+    setCommitsResult(null);
+  };
+
+  const alertFn = (msg, type) => {
+    setAlert({
+      message: msg,
+      type,
+      show: true,
+    });
+    interval.current = setTimeout(() => {
+      setAlert({
+        message: msg,
+        type,
+        show: false,
+      });
+      clearInterval(interval.current);
+    }, 2000);
+  };
+
+  useEffect(() => {
+    if (alert.show === false) {
+      clearTimeout(interval.current);
+    }
+  }, [alert]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Pick Commits</header>
+      <main>
+        <div className="container">
+          {alert.show && (
+            <span className={`alert alert-${alert.type}`}>{alert.message}</span>
+          )}
+          {commitResults === null && (
+            <div className="authorInput">
+              <img alt="Git Logo" width={90} height={90} src={icon} />
+              <input
+                autoFocus={true}
+                onChange={handleOnChangeFn}
+                onKeyUp={handleKeyUpFn}
+                placeholder="Type Commit Author here..."
+              />
+
+              <button disabled={loading} onClick={handleSearchFn}>
+                {loading
+                  ? "Getting commits from author..."
+                  : "Get Commits from Author"}
+              </button>
+            </div>
+          )}
+          {commitResults !== null && (
+            <ShowCommits
+              alertFn={alertFn}
+              goBack={goBack}
+              commitResultsData={commitResults}
+            />
+          )}
+        </div>
+      </main>
     </div>
   );
 }
